2b90c9e28c6a8a172d42c66d285a1b4fca701633
lehuy121 <lehuy121@gmail.com>
2020-10-06 21:05:22 +0700

    30370_sign_up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,36 @@
 class UsersController < ApplicationController
+  before_action :find_user, only: [:show]
+
+  def show; end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      @user.send_activation_email
+      UserMailer.account_activation(@user).deliver_now
+      flash[:info] = t "form.create"
+      redirect_to root_url
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:full_name, :email, :password,
+                                 :password_confirmation)
+  end
+
+  def find_user
+    @user = @user = User.find_by id: params[:id]
+    return if @user
+
+    flash[:danger] = t "form.found"
+    redirect_to signup_path
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,11 @@ Rails.application.routes.draw do
   scope "(:locale)", locale: /en|vi/ do
     root "sessions#new"
     get "/home", to: "static_pages#home"
+    get "/signup", to: "users#new"
     get "/login", to: "sessions#new"
     post "/login",  to: "sessions#create"
     delete "/logout", to: "sessions#destroy"
+    resources :users
+    resources :account_activations, only: [:edit]
   end
 end
