06cdbd3219e9d26c891555cfdb6d5f45033a9907
leviethung123 <levan999mt@gmail.com>
2020-10-07 15:58:20 +0700

    Sign in/Sign out

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   scope "(:locale)", locale: /en|vi/ do
+    root "sessions#new"
     get "/home", to: "static_pages#home"
+    get "/login", to: "sessions#new"
+    post "/login",  to: "sessions#create"
+    delete "/logout", to: "sessions#destroy"
   end
 end
