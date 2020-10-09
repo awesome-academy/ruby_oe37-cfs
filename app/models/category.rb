3cace47bd68b2eacc30d34b0a331dc72f25dc3e6
lehuy121 <lehuy121@gmail.com>
2020-10-09 09:32:40 +0700

    30591_list_all_plan: fix comment

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,6 @@
 class Category < ApplicationRecord
   belongs_to :user
   has_many :plans, dependent: :destroy
+
   enum delete_flag: {activate: 0, inactive: 1}
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
     delete "/logout", to: "sessions#destroy"
     resources :users
     resources :account_activations, only: [:edit]
-    resources :plan, only: [:new , :create]
+    resources :plan, only: [:index, :new , :create]
     resources :password_resets, only: [:new, :create, :edit, :update]
     resources :categories, only: [:index, :create, :destroy]
   end
