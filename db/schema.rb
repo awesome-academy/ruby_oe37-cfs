e1cbd66748bcce96679fe0f3c9b55a3170c22aae
lehuy121 <lehuy121@gmail.com>
2020-10-09 09:32:40 +0700

    30548_init_model_add_relationship

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,33 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_07_062517) do
+ActiveRecord::Schema.define(version: 2020_10_09_021405) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.boolean "delete_flag"
+    t.integer "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id", "created_at"], name: "index_categories_on_user_id_and_created_at"
+    t.index ["user_id"], name: "index_categories_on_user_id"
+  end
+
+  create_table "plans", force: :cascade do |t|
+    t.float "budget"
+    t.integer "month"
+    t.integer "spending_category"
+    t.integer "type"
+    t.integer "status"
+    t.float "moneys"
+    t.integer "user_id", null: false
+    t.integer "category_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["category_id"], name: "index_plans_on_category_id"
+    t.index ["user_id", "month"], name: "index_plans_on_user_id_and_month"
+    t.index ["user_id"], name: "index_plans_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "full_name"
@@ -28,4 +54,7 @@ ActiveRecord::Schema.define(version: 2020_10_07_062517) do
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
+  add_foreign_key "categories", "users"
+  add_foreign_key "plans", "categories"
+  add_foreign_key "plans", "users"
 end
