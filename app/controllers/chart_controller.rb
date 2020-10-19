67802803a745fd315959fc6757c7af8732d99334
leviethung123 <levan999mt@gmail.com>
2020-10-19 13:04:36 +0700

    update chart

---
+++ app/controllers/chart_controller.rb
@@ -0,0 +1,18 @@
+class ChartController < ApplicationController
+  before_action :find_plan_id_user, only: [:show, :index]
+
+  def index; end
+
+  def show
+    @income = @plans.group(:spending_category).group(:month)
+      .income.sum(:moneys)
+    @expenses = @plans.group(:spending_category).group(:month)
+      .expenses.sum(:moneys)
+  end
+
+  private
+
+  def find_plan_id_user
+    @plans = current_user.plans
+  end
+end
